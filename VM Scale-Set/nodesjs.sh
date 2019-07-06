42cb4c8a27c0df9a90319af70089f002427c6f3c
SmithMMTK <smithm@hotmail.co.th>
2019-07-06 23:00:13 +0700

    Update nodesjs.sh

--- VM Scale-Set/nodesjs.sh
+++ VM Scale-Set/nodesjs.sh
@@ -1,6 +1,7 @@
 sudo apt-get update --yes
 sudo apt-get install nodejs --yes
 sudo apt-get install npm --yes
+cd "/home/azureuser"
 rm -rf nodejs_express    
 git clone https://github.com/SmithMMTK/nodejs-express
 cd nodejs-express
