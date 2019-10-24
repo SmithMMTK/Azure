eaa7045c3c48518157c2838bbdc37070e245e52f
SmithMMTK <smithm@hotmail.co.th>
2019-10-24 11:31:03 +0700

    Update installnodejs.sh

--- scripts/installnodejs.sh
+++ scripts/installnodejs.sh
@@ -1,4 +1,7 @@
+cd ~/
 git clone https://github.com/SmithMMTK/nodejs-express.git
 sudo apt-get update -y
 sudo apt-get install nodejs -y
 sudo apt-get install npm -y
+cd ~/nodejs-express
+nodejs index.js
