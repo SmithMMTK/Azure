1ade8dfd13a7018c680622b8ad7f2dcbfad99e5d
SmithMMTK <smithm@hotmail.co.th>
2019-10-24 12:02:59 +0700

    Update installnodejs.sh

--- scripts/installnodejs.sh
+++ scripts/installnodejs.sh
@@ -1,15 +1,15 @@
-echo "Start Custom Script." >> /home/azureuser/boot_log.txt
-echo "Change directory." >> /home/azureuser/boot_log.txt
+echo "Start Custom Script." >> /home/boot_log.txt
+echo "Change directory." >> /home/boot_log.txt
 cd ~/
-echo "Start cloning Git." >> /home/azureuser/boot_log.txt
+echo "Start cloning Git." >> /home/boot_log.txt
 git clone https://github.com/SmithMMTK/nodejs-express.git
-echo "Update APT." >> /home/azureuser/boot_log.txt
+echo "Update APT." >> /home/boot_log.txt
 sudo apt-get update -y
-echo "Install Nodejs." >> /home/azureuser/boot_log.txt
+echo "Install Nodejs." >> /home/boot_log.txt
 sudo apt-get install nodejs -y
-echo "Install Npm." >> /home/azureuser/boot_log.txt
+echo "Install Npm." >> /home/boot_log.txt
 sudo apt-get install npm -y
-echo "Change to Nodejs-express directory." >> /home/azureuser/boot_log.txt
+echo "Change to Nodejs-express directory." >> /home/boot_log.txt
 cd ~/nodejs-express
-echo "Run Nodejs" >> /home/azureuser/boot_log.txt
+echo "Run Nodejs" >> /home/boot_log.txt
 nodejs index.js
